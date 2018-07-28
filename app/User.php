11fbf22416b2d724ab9b53aaab9292a68013344c
Edwin Encomienda <encomienda000@gmail.com>
2018-07-28 16:18:14 +0800

    pivot tables

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function isAdmin()
+    {
+        return false; // for testing
+    }
 }
